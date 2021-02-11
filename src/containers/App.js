eb897a1ba490150bbb5a9a9e943c9a07a8db093c
Placido Wang <placidow@gmail.com>
2021-02-11 01:04:16 -0600

    adding api fetch and pushing through ssh

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,23 @@
 // import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 import StockTile from '../components/StockTile.js';
 
+
 function App() {
+  const KEY = process.env.REACT_APP_AV_KEY;
+  const [portfolio, setPortfolio] = useState([]);
+  const [data, setData] = useState({})
+
+  // Will need to set up Cleanup when/if auto-refreshing ticker data
+  useEffect(() => {
+    fetch('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=GME&interval=5min&apikey=' + KEY)
+      .then(r => r.json())
+      .then(d => setData(d))
+      .then(console.log(data))
+  },[])
+
   return (
     <div className="App">
       <StockTile />
